aaed092f36ac166cb6ea8e52b8c1d94a2638830e
Gopi <59192600+GopiPacha1996@users.noreply.github.com>
2020-10-13 06:37:12 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Multi-Docker-Container-Apllication
+                    Multi-Container-Laravel-App
                 </div>
 
                 {{-- <div class="links">
